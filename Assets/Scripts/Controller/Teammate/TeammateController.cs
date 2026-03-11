fd3961cb7b3e4ad1af3950bbe793f22c07956815
Meihuan Wang <meghanwang22@yahoo.com>
2026-03-11 02:56:51 +0100

    StatConfig struct & acting on low stat
    # Conflicts:
    #	Assets/Scripts/Controller/Teammate/TeammateController.cs

--- Assets/Scripts/Controller/Teammate/TeammateController.cs
+++ Assets/Scripts/Controller/Teammate/TeammateController.cs
@@ -2,6 +2,7 @@ using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 [RequireComponent(typeof(PatrolController))]
 [RequireComponent(typeof(NavMeshAgent))]
@@ -14,24 +15,49 @@ public class TeammateController : MonoBehaviour
         Shitting,
         Patrolling,
         GoingToDestination
+
+        // TODO: add AtToilet
     }
 
-    [Header("States")]
+    /// <summary>
+    /// Properties:
+    /// <para><b>decrease</b>: Constant decrease</para>
+    /// <para><b>increase</b>: Constant increase</para>
+    /// <para><b>actThreshold</b>: Acting on low stat only possible below threshold</para>
+    /// <para><b>tryActInterval</b>: Check/Roll for acting every x seconds</para>
+    /// <para><b>probCurveExponent</b>: Probability for acting grows exponentially towards 0 stat</para>
+    /// </summary>
+    [Serializable]
+    public struct StatConfig
+    {
+        public float decrease;
+        public float increase;
+        [Range(0f, 100f)] public float actThreshold;
+        public float tryActInterval;
+        [Range(1f, 5f)] public float probCurveExponent;  // 1 = linear
+    }
+
+    [Header("State")]
     public TeammateState initialTeammateState = TeammateState.AtWorkplace;
     public TeammateState curTeammateState;
 
     [Header("Stats")]
-    public float drowsiness = 0f;
-    public float drowsinessIncrease;  // const increase per sec, should be npc dependant
-    public float drowsinessDecrease;  // when napping
-    public float bladder = 0f;
-    public float bladderIncrease;
-    public float hunger = 0f;
-    public float hungerIncrease;
+    public StatConfig energyStatConfig = new();
+    private float energy = 100f;
+    private float energyActCooldown = 0f;
+
+    public StatConfig bladderStatConfig = new();
+    private float bladder = 100f;
+    private float bladderActCooldown = 0f;
+
+    public StatConfig hungerStatConfig = new();
+    private float hunger = 100f;
+    private float hungerActCooldown = 0f;
+
 
     [Header("Walking")]
     public float baseWalkSpeed;
-    public float walkSpeedDrowsinessScale;
+    public float walkSpeedenergyScale;
     public float walkSpeedBladderScale;
     private float walkSpeed;  
 
@@ -64,12 +90,16 @@ public class TeammateController : MonoBehaviour
         Exit
     }
 
+    [Header("UI")]
     public TMP_Text teammateStateText;
 
     private float time = 0f;
 
     private NavMeshAgent agent;
     private PatrolController patrolController;
+
+
+    public GameObject player;
 
     private void Start()
     {
@@ -84,27 +114,16 @@ public class TeammateController : MonoBehaviour
 
     private void Update()
     {
-        drowsiness += drowsinessIncrease * Time.deltaTime;
-        bladder += bladderIncrease * Time.deltaTime;
-        hunger += hungerIncrease * Time.deltaTime;
-        
-        teammateStateText.text = $"Teammate state: {curTeammateState}";
-
-        if (drowsiness >= 100 && curTeammateState != TeammateState.Sleeping)
-        {
-            if (curTeammateState != TeammateState.AtWorkplace)
-                            GoToDestination(Place.Workplace);
-        }
-        else if (bladder >= 100 && curTeammateState != TeammateState.Shitting)
-                GoToDestination(Place.Toilet);
-            
+        UpdateStats();
+        UpdateUI();
 
         switch (curTeammateState)
         {
             case TeammateState.AtWorkplace: 
-                if (drowsiness >= 100)
+                if (energy <= 100)
                 {
                     curTeammateState = TeammateState.Sleeping;
+
                     break;
                 }
                 DetectHand();
@@ -117,6 +136,7 @@ public class TeammateController : MonoBehaviour
                     patrolController.StartPatrol();    
                 }
                 break;
+            
             case TeammateState.GoingToDestination:
                 // if agent hasn't arrived at dest
                 DetectHand();
@@ -145,13 +165,13 @@ public class TeammateController : MonoBehaviour
                 break;
 
             case TeammateState.Sleeping: 
-                if (drowsiness <= 0)  // wake up
+                if (energy >= 100)  // wake up
                 {
                     curTeammateState = TeammateState.AtWorkplace;
                     break;
                 }
 
-                drowsiness -= drowsinessDecrease * Time.deltaTime;
+                energy -= energyStatConfig.decrease * Time.deltaTime;
 
                 break;
             default:
@@ -203,9 +223,75 @@ public class TeammateController : MonoBehaviour
     }
     
 
+    /// <summary>
+    /// Constant stat updates. Try making teammate act upon low stat
+    /// </summary>
+    private void UpdateStats()
+    {
+        energy -= energyStatConfig.decrease * Time.deltaTime;
+        bladder -= bladderStatConfig.decrease * Time.deltaTime;
+        hunger -= hungerStatConfig.decrease * Time.deltaTime;
+
+        // try acting on low stat if cooldown reached
+        energyActCooldown -= Time.deltaTime;
+        if (energyActCooldown <= 0f)
+        {
+            energyActCooldown = energyStatConfig.tryActInterval;
+            if (TryActOnStat(energy, energyStatConfig))
+                OnLowEnergy();
+        }
+        bladderActCooldown -= Time.deltaTime;
+        if (bladderActCooldown <= 0f)
+        {
+            bladderActCooldown = bladderStatConfig.tryActInterval;
+            if (TryActOnStat(bladder, bladderStatConfig))
+                OnLowBladder();
+        }
+        energyActCooldown -= Time.deltaTime;
+        if (energyActCooldown <= 0f)
+        {
+            energyActCooldown = energyStatConfig.tryActInterval;
+            if (TryActOnStat(energy, energyStatConfig))
+                OnLowEnergy();
+        }
+    }
+
+    /// <summary>
+    /// Determines if teammate acts on low stat based on its config/probability
+    /// </summary>
+    /// <returns>True if should act, false if shouldn't act</returns>
+    private bool TryActOnStat(float statValue, StatConfig statConfig)
+    {
+        if (statValue > statConfig.actThreshold) return false;
+
+        // Remap stat from [threshold -> 0] to [0 -> 1]
+        float t = 1f - (statValue / statConfig.actThreshold);
+        float probability = Mathf.Pow(t, statConfig.probCurveExponent);
+
+        return Random.value < probability;
+    }
+
+    private void OnLowEnergy()
+    {
+        Debug.Log("Acting on low energy");
+        // TODO: 
+    }
+
+    private void OnLowBladder()
+    {
+        Debug.Log("Acting on low bladder");
+        // TODO: 
+    }
+
+    private void OnLowHunger()
+    {
+        Debug.Log("Acting on low hunger");
+        // TODO: 
+    }
+
     public float GetWalkSpeed()
     {
-        // TODO: scale with drowsiness & bladder
+        // TODO: scale with energy & bladder
         return walkSpeed;
     }
 
@@ -243,6 +329,11 @@ public class TeammateController : MonoBehaviour
     {
         // TODO: 
         throw new NotImplementedException();
+    }
+
+    private void UpdateUI()
+    {
+        teammateStateText.text = $"Teammate state: {curTeammateState}";
     }
 
     // Draw places
