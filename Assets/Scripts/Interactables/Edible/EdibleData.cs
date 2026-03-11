45bad5b94cf6650fb0515a9bdc53c495bdfeb0dc
Alexandra Menshikova <ochlinetime@gmail.com>
2026-03-11 11:05:58 +0100

    change edible params to floats

--- Assets/Scripts/Interactables/Edible/EdibleData.cs
+++ Assets/Scripts/Interactables/Edible/EdibleData.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class EdibleData : ScriptableObject
 {
     public EdibleType type;
-    public int bladder;
-    public int hunger;
-    public int energy;
+    public float bladder;
+    public float hunger;
+    public float energy;
     
     [Serializable]
     public enum EdibleType
