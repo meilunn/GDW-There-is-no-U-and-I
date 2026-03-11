3e4914faf62060560fa5cfde046c53622e42cdd2
FixlTV <74925250+FixlTV@users.noreply.github.com>
2026-03-11 03:22:49 +0100

    Project Progress added

--- Assets/Scripts/GameLogic/GameManager.cs
+++ Assets/Scripts/GameLogic/GameManager.cs
@@ -35,6 +35,7 @@ public class GameManager : MonoBehaviour {
 	private float progressMeter = 0;  // 0-100
 	private float requiredProgressPerDay;
 	private float progressMadeToday = 0;
+	public ProjectProgress projectProgress;
 
 	// public List<Task> tasksOfTheDay = new();
 
@@ -66,9 +67,11 @@ public class GameManager : MonoBehaviour {
 	}
 
 	float CalculateSusIncrease() {
-		float modifier = Mathf.Pow(100f / MaxDays / requiredProgressPerDay, 2) - 1f;
-		float susIncrease = modifier * Mathf.Pow(curDay, 2.8f);
-		return Mathf.Max(0f, Mathf.Min(100f, susIncrease));
+		return 0;
+		// das ist irgendwie bullshit
+		// float modifier = Mathf.Pow(100f / MaxDays / requiredProgressPerDay, 2) - 1f;
+		// float susIncrease = modifier * Mathf.Pow(curDay, 2.8f);
+		// return Mathf.Max(0f, Mathf.Min(100f, susIncrease));
 	}
 
 	IEnumerator OnDayEnd() {
