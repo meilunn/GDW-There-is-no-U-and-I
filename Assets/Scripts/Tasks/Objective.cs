46463dfae075d5cb48fa2be0a7ebd2d28b802287
Victor Apostol <victor.apostol@tum.de>
2026-03-11 19:45:37 +0100

    Dialogues

--- Assets/Scripts/Tasks/Objective.cs
+++ Assets/Scripts/Tasks/Objective.cs
@@ -6,8 +6,8 @@ public enum ObjectiveId {
 }
 
 [CreateAssetMenu(fileName = "New Objective", menuName = "Quests/Objective")]
-public class Objective : ScriptableObject {
-
+public class Objective : ScriptableObject 
+{
 	public ObjectiveId id;
 	public string description;
 	public bool ownerRequired;
