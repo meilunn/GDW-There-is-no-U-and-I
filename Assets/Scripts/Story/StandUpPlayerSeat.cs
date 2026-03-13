2e75c9bd2e33bcc8eca7381635f0258dec791843
Victor Apostol <victor.apostol@tum.de>
2026-03-13 19:33:31 +0100

    fonts

--- Assets/Scripts/Story/StandUpPlayerSeat.cs
+++ Assets/Scripts/Story/StandUpPlayerSeat.cs
@@ -1,10 +1,13 @@
+using UnityEngine;
+
 public class StandUpPlayerSeat : BaseStandUpSeat {
 	public override void Release() {
 		Player.Instance.RemoveMoveLock(gameObject);
 	}
 
 	public override void Sit() {
-		Player.Instance.transform.SetPositionAndRotation(transform.position, transform.rotation);
+		Vector3 seatPosition = transform.position + Vector3.up;
+		Player.Instance.transform.SetPositionAndRotation(seatPosition, transform.rotation);
 		Player.Instance.AddMoveLock(gameObject);
 	}
 }
